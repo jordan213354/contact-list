089c9ccea44af4a5dfa5a0bfd42289e9d472b40b
jordan aranda <xavieraranda123@gmail.com>
2023-03-17 17:45:55 -0500

    entregable

---
+++ pra.js
@@ -0,0 +1,47 @@
+
+
+
+
+let user1;
+let user2;
+let user3;
+
+let registro;
+
+
+
+const contactos = [
+     user1 = {
+        nombre: "jordan",
+        apellido: "aranda",
+        numero: 3153716559,
+        email: "iksfjfok@gmail.com"
+    }, user2 = {
+        nombre: "camilo",
+        apellido: "gomez",
+        numero: 3566655,
+        email: "iksfjfok@gmail.com"
+    }, user3 = {
+        nombre: "carlos",
+        apellido: "fernandez",
+        numero: 3153716559,
+        email: "iksfjfok@gmail.com"
+    },
+user4 = {
+        nombre: "maria",
+        apellido: "muños",
+        numero: 3153716559,
+        email: "iksfjfok@gmail.com"
+    }];
+
+contactos.push(
+    {
+        nombre: "xavier",
+        apellido: "astudillo",
+        numero: "566566",
+        email: "dfnslgjriohg@gmail.com"
+    }
+);
+
+//contactos.pop();
+console.log(contactos)
