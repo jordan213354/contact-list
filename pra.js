fceeb4d7a1f65df01a6a9f468a77593ce3a9ec41
jordan aranda <xavieraranda123@gmail.com>
2023-04-26 16:17:01 -0500

    trabajo hecho

--- pra.js
+++ pra.js
@@ -1,47 +1,27 @@
+const listaDeContactos = [];
+
+// AÑADIR CONTACTO
+
+function añadirContacto(contacto) {
+    listaDeContactos.push (contacto)
+};
+añadirContacto("jordan aranda");
+añadirContacto("carlos flor");
+añadirContacto("sara ramires");
+añadirContacto("camilo gomez");
+
+console.log(listaDeContactos)
 
 
+// ELIMINAR CONTACTO
 
-
-let user1;
-let user2;
-let user3;
-
-let registro;
-
-
-
-const contactos = [
-     user1 = {
-        nombre: "jordan",
-        apellido: "aranda",
-        numero: 3153716559,
-        email: "iksfjfok@gmail.com"
-    }, user2 = {
-        nombre: "camilo",
-        apellido: "gomez",
-        numero: 3566655,
-        email: "iksfjfok@gmail.com"
-    }, user3 = {
-        nombre: "carlos",
-        apellido: "fernandez",
-        numero: 3153716559,
-        email: "iksfjfok@gmail.com"
-    },
-user4 = {
-        nombre: "maria",
-        apellido: "muños",
-        numero: 3153716559,
-        email: "iksfjfok@gmail.com"
-    }];
-
-contactos.push(
-    {
-        nombre: "xavier",
-        apellido: "astudillo",
-        numero: "566566",
-        email: "dfnslgjriohg@gmail.com"
-    }
-);
-
-//contactos.pop();
-console.log(contactos)
+function eliminarContacto(nombre) {
+    listaDeContactos.splice(2, 1);
+    
+};
+// eliminarContacto();
+ 
+function actulizarContacto() {
+    listaDeContactos.splice(2,0, "camilo fuentes")
+};
+// actulizarContacto();
